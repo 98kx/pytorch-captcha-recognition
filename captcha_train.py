a610c130b6bc162585ae6e0b8892be89fac87173
tom <tom@gmail.com>
2022-11-19 22:45:43 +0800

    mm

--- captcha_train.py
+++ captcha_train.py
@@ -11,12 +11,35 @@ batch_size = 100
 learning_rate = 0.001
 
 def main():
-    cnn = CNN()
+
+
+    use_cuda = torch.cuda.is_available()
+    use_mps = torch.backends.mps.is_available()
+
+
+    if use_cuda:
+        device = torch.device("cuda")
+        print("device use cuda" )
+    elif use_mps:
+        device = torch.device("mps")
+        print("device use mps")
+    else:
+        device = torch.device("cpu")
+        print("device use cpu")
+
+    train_kwargs = {'batch_size': batch_size}
+
+    if use_cuda:
+        cuda_kwargs = {'num_workers': 1,
+                       'pin_memory': True,
+                       'shuffle': True}
+        train_kwargs.update(cuda_kwargs)
+
+    cnn = CNN().to(device)
+
     cnn.train()
     print('init net')
-    device = torch.device("mps")
-    print("use mps :", torch.has_mps)
-
+    # model = CNN().to(device)
     criterion = nn.MultiLabelSoftMarginLoss().to(device)
     optimizer = torch.optim.Adam(cnn.parameters(), lr=learning_rate)
 
@@ -24,12 +47,13 @@ def main():
     train_dataloader = my_dataset.get_train_data_loader()
     for epoch in range(num_epochs):
         for i, (images, labels) in enumerate(train_dataloader):
+            images, labels = images.to(device), labels.to(device)
             images = Variable(images)
             labels = Variable(labels.float())
             predict_labels = cnn(images)
             # print(predict_labels.type)
             # print(labels.type)
-            loss = criterion(predict_labels, labels)
+            loss = criterion(predict_labels , labels )
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
